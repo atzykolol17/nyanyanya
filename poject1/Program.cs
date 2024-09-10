b2652842cf1744c4e6d4488c396fc37b40f66609
atzyko <dfgdfg@gmail.com>
2024-09-10 11:49:33 +0300

    jlk;lk;l

--- poject1/Program.cs
+++ poject1/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Priv Natysha");
+//Console.WriteLine("Priv Natysha");
+Console.Write("Enter miles: ");
+Console.ReadLine();
+float miles = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Result: {miles * 1.60934} km ");
