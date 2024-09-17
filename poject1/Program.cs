0c1c784c6697bdc080866798772b54508d802bf1
atzyko <dfgdfg@gmail.com>
2024-09-17 11:42:19 +0300

    ghhdfhd

--- poject1/Program.cs
+++ poject1/Program.cs
@@ -1,8 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 //Console.WriteLine("Priv Natysha");
-Console.Write("Enter miles: ");
-Console.ReadLine();
-float miles = float.Parse(Console.ReadLine());
+//Console.Write("Enter miles: ");
+//Console.ReadLine();
+//float miles = float.Parse(Console.ReadLine());
 
-Console.WriteLine("Result: {miles * 1.60934} km ");
+//Console.WriteLine("Result: {miles * 1.60934} km ");
+
+
+Console.WriteLine("-----------weather----------");
+Console.WriteLine("Entr curent degree ");
+int degree = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{degree*1.8 + 32}");
